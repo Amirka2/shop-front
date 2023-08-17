77a07ed1a9c72a8de46d87df398fccac1e8b9b55
Amir Fayzullin <far20022002@mail.ru>
2023-08-17 15:49:31 +0500

    added routing

--- src/app/shop/App.tsx
+++ src/app/shop/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Route, BrowserRouter, Routes, Link} from 'react-router-dom';
 
+const Test = function() {
+    return (
+        <div>test component</div>
+    )
+}
 export function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <BrowserRouter>
+        <div className="App">
+            <div>
+                <Link to='/'>main</Link>
+                <br/>
+                <Link to='/test'>test</Link>
+            </div>
+            <Routes>
+                <Route path='/test' Component={Test}></Route>
+                <Route path='/'/>
+            </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
