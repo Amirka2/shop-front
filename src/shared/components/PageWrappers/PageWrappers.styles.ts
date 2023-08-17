3295941d8b5f837905048d16ecf3ef27ac01cbdf
Amir Fayzullin <far20022002@mail.ru>
2023-08-18 01:28:46 +0500

    added alignments to page template and footer

--- src/shared/components/PageWrappers/PageWrappers.styles.ts
+++ src/shared/components/PageWrappers/PageWrappers.styles.ts
@@ -3,6 +3,8 @@ import {styled} from "styled-components";
 export const MainWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   min-height: 500px;
   margin: 0 0 20px 0;
--- src/shared/footer/ui/Footer.styles.ts
+++ src/shared/footer/ui/Footer.styles.ts
@@ -2,9 +2,14 @@ import {styled} from "styled-components";
 import {Colors} from '@/shared/constants'
 
 export const Wrapper = styled.div`
-  width: 100%;
+  width: 100vw;
   min-height: 630px;
   height: 630px;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   background-color: ${Colors.blue};
   color: ${Colors.white};
 `;
