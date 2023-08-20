400adb8eb9b76f3f1fab1378a0b6789032317eaa
Amir Fayzullin <far20022002@mail.ru>
2023-08-20 18:07:09 +0500

    refactored navbar for adaptive view

--- src/shared/navbar/ui/NavBar.styles.ts
+++ src/shared/navbar/ui/NavBar.styles.ts
@@ -2,21 +2,31 @@ import {styled} from "styled-components";
 import {NavLink} from "react-router-dom";
 import {Colors} from "@/shared/constants";
 
-export const Wrapper = styled.div`
-  width: 1330px;
-  min-height: 50px;
-  height: 50px;
+export const Wrapper = styled.div<{
+    $isMobile: boolean;
+}>`
+  width: ${props => props.$isMobile ? '70vw' : '1330px'};
+  min-height: ${props => props.$isMobile ? '' : '50px'};
+  height: ${props => props.$isMobile ? '' : '50px'};
   margin: 0 0 20px 0;
   display: flex;
-  flex-direction: row;
+  flex-direction: ${props => props.$isMobile ? 'column' : 'row'};
   justify-content: space-evenly;
   align-items: center;
   background-color: ${Colors.gray};
   border-radius: 10px;
 `;
 
-export const Link = styled(NavLink)`
+export const Link = styled(NavLink)<{
+    $isMobile: boolean;
+}>`
+  width: ${props => props.$isMobile ? '100%' : ''};
+  text-align: ${props => props.$isMobile ? 'center' : ''};
   color: ${Colors.black};
   text-decoration: none;
   font-size: 24px;
+  &:hover {
+    background-color: ${Colors.blue};
+    color: ${Colors.white};
+  }
 `;
