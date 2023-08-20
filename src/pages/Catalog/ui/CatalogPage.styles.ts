d5bb02e79433d12f9c94521b5a57cb76d84a5dc8
Amir Fayzullin <far20022002@mail.ru>
2023-08-20 17:54:35 +0500

    refactored adaptive view

--- src/pages/Catalog/ui/CatalogPage.styles.ts
+++ src/pages/Catalog/ui/CatalogPage.styles.ts
@@ -4,7 +4,7 @@ import {MainWrapper} from "@/shared/components";
 export const Wrapper = styled(MainWrapper)<{
     $isMobile: boolean;
 }>`
-  width: ${props => props.$isMobile ? '100%' : '1330px'};
+  width: ${props => props.$isMobile ? '90vw' : '1330px'};
   justify-content: ${props => props.$isMobile ? 'flex-start' : 'space-between'};
   align-items: ${props => props.$isMobile ? 'center' : 'flex-start'};
   flex-direction: ${props => props.$isMobile ? 'column' : 'row'};
