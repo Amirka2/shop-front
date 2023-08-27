74e3eae63a899a3f5200acab5fcd1323be327fa0
Amir Fayzullin <far20022002@mail.ru>
2023-08-27 22:28:45 +0500

    added UI for categories

--- src/shared/components/Contacts/Contacts.styles.ts
+++ src/shared/components/Contacts/Contacts.styles.ts
@@ -1,5 +1,4 @@
 import {styled} from "styled-components";
-import {Colors} from "@/shared/constants";
 
 export const Wrapper = styled.div`
   height: 100%;
--- src/shared/components/Logo/index.tsx
+++ src/shared/components/Logo/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Logo = () => {
     return (
-        <img src='icons/logo.png' alt='mitcad doo logo'/>
+        <img src={'icons/logo.png'} alt='mitcad doo logo'/>
     );
 };
 
