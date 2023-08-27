b733554ecb0d3416ebfd7a36c284bf4ebcf97370
Amir Fayzullin <far20022002@mail.ru>
2023-08-27 21:35:34 +0500

    copied main page as catalog

--- src/pages/Main/ui/MainPage.styles.ts
+++ src/pages/Main/ui/MainPage.styles.ts
@@ -1,6 +1,11 @@
 import {styled} from "styled-components";
 import {MainWrapper} from "@/shared/components";
 
-export const Wrapper = styled(MainWrapper)`
-  
+export const Wrapper = styled(MainWrapper)<{
+    $isMobile: boolean;
+}>`
+  width: ${props => props.$isMobile ? '90vw' : '1330px'};
+  justify-content: ${props => props.$isMobile ? 'flex-start' : 'space-between'};
+  align-items: ${props => props.$isMobile ? 'center' : 'flex-start'};
+  flex-direction: ${props => props.$isMobile ? 'column' : 'row'};
 `;
--- src/pages/Main/ui/MainPage.tsx
+++ src/pages/Main/ui/MainPage.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import {Wrapper} from "./MainPage.styles";
+import {useMobileOrDesktop} from "@/shared/libs/hooks";
+import {items} from "@/pages/Catalog/mock";
+import {ItemsGrid, Product} from "@/shared/components";
 
-export const MainPage = () => {
+interface MainPageProps {
+    itemsValue: number;
+    isMobile: boolean;
+}
+
+export const MainPage = (props: MainPageProps) => {
+    let isMobile = useMobileOrDesktop();
+    let width = isMobile ? 600 : 1000;
+    const itemsComponents = items.slice(0, props.itemsValue).map(i => <Product {...i}/>);
     return (
-        <Wrapper>
+        <Wrapper $isMobile={props.isMobile}>
             <h1>Main Page</h1>
+            <ItemsGrid height={3000} width={width}>
+                {itemsComponents}
+            </ItemsGrid>
         </Wrapper>
     );
 };
