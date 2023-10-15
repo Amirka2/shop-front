7c0f18821cb515f9d3656f3a5a4b3f45d019ba11
vllllktoria <vika.v1ktoria@mail.ru>
2023-10-15 19:29:52 +0500

    feature_1: changed shipping page text to dick

--- src/pages/Shipping/ui/ShippingPage.tsx
+++ src/pages/Shipping/ui/ShippingPage.tsx
@@ -3,7 +3,7 @@ import {Wrapper} from './ShippingPage.styles';
 export const ShippingPage = () => {
     return (
         <Wrapper>
-            <h1>Страница находится в разработке</h1>
+            <h1>ХУЙ находится в разработке</h1>
         </Wrapper>
     );
 };
