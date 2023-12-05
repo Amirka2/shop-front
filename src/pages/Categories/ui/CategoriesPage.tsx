b8b176e4685231bd93577b30e79be30f8c0c7940
Amir Fayzullin <far20022002@mail.ru>
2023-12-05 18:52:15 +0500

    feat: added store for categories

--- src/pages/Categories/ui/CategoriesPage.tsx
+++ src/pages/Categories/ui/CategoriesPage.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {Wrapper} from "./CategoriesPage.styles";
 
 import {ItemsGrid} from "@/shared/components";
-import {categories} from '@/app/shop/mock';
-import {useMobileOrDesktop} from "@/shared/hooks";
+import {useMobileOrDesktop, useStores} from "@/shared/hooks";
 import {CategoryCard} from "@/entities";
+import {getCategories} from '../api'
+import {observer} from "mobx-react";
 
-export const CategoriesPage = () => {
-    let isMobile = useMobileOrDesktop();
-    let width = isMobile ? 600 : 1000;
-    const itemsComponents = categories.map(c => <CategoryCard {...c}/>);
-    return (
-        <Wrapper $isMobile={isMobile}>
-            <h1>Категории</h1>
-            <ItemsGrid width={width.toString()}>
-                {itemsComponents}
-            </ItemsGrid>
-        </Wrapper>
-    );
-};
+export const CategoriesPage = observer(() => {
+  const {categoriesStore} = useStores();
+  let isMobile = useMobileOrDesktop();
+
+  const {categories} = categoriesStore;
+
+  useEffect(() => {
+    const response = getCategories();
+    response.then(result => {
+      categoriesStore.set(result);
+    })
+  }, [])
+
+  let width = isMobile ? 600 : 1000;
+  const itemsComponents = categories.map(c => <CategoryCard {...c}/>);
+  return (
+    <Wrapper $isMobile={isMobile}>
+      <h1>Категории</h1>
+      <ItemsGrid width={width.toString()}>
+        {itemsComponents}
+      </ItemsGrid>
+    </Wrapper>
+  );
+});
