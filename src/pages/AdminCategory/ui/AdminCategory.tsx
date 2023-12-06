8e53b398f216fe97fbf3f5d3a19c61704b8911d2
Amir Fayzullin <far20022002@mail.ru>
2023-12-06 15:46:29 +0500

    fix: fixed layout

--- src/pages/AdminCategory/ui/AdminCategory.tsx
+++ src/pages/AdminCategory/ui/AdminCategory.tsx
@@ -3,76 +3,83 @@ import React, {useEffect, useRef, useState} from 'react';
 import {Container} from "@/shared/components";
 
 import {CATEGORIES, createCategory} from '../api';
-import * as Styles from './AdminCategory.styles';
 import {SubCategories} from "./SubCategories";
 import {subCategories as SubCategoriesData} from "@/app/shop/mock";
 import {Editor} from "./Editor";
 import {useNavigate} from "react-router";
 import {Back} from "@/shared/ui";
 
+import * as Styles from './AdminCategory.styles';
+
 export const AdminCategory = () => {
-    const [isEditorOpen, setEditorOpen] = useState(false);
-    const [categoryName, setCategoryName] = useState('');
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCategoryName(e.target.value);
+  const [isEditorOpen, setEditorOpen] = useState(false);
+  const [categoryName, setCategoryName] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCategoryName(e.target.value);
+  }
+  const handleSave = () => {
+    createCategory({
+      title: categoryName,
+    });
+    setCategoryName('');
+    setEditorOpen(false);
+  }
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      handleSave();
     }
-    const handleSave = () => {
-        createCategory({
-            title: categoryName,
-        });
-        setCategoryName('');
-        setEditorOpen(false);
-    }
-    const reloadRef = useRef(null);
-    useEffect(() => {
-        console.log('rerendered');
-    }, [reloadRef])
+  };
+  const reloadRef = useRef(null);
+  useEffect(() => {
+    console.log('rerendered');
+  }, [reloadRef])
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <main>
-            <Container>
-                <Styles.Wrapper>
-                    <Styles.BackButton onClick={() => navigate(-1)}>
-                        <Back />
-                    </Styles.BackButton>
+  return (
+    <Styles.Wrapper>
+      <Container>
 
-                    <Styles.ContentWrapper>
-                        <Styles.AddCategoryWrapper>
-                            <Styles.AddCategory onClick={() => setEditorOpen(prev => !prev)}>
-                                {isEditorOpen ? '-' : '+'}
-                            </Styles.AddCategory>
-                        </Styles.AddCategoryWrapper>
+        <Styles.BackButton onClick={() => navigate(-1)}>
+          <Back/>
+        </Styles.BackButton>
 
-                        <Styles.Categories>
-                            {CATEGORIES.map(category => {
-                                return (
-                                    <Styles.Category>
-                                        <Styles.Title>
-                                            {category.title}
-                                        </Styles.Title>
-                                        <SubCategories
-                                            categoryId={category.id}
-                                            subCategories={SubCategoriesData}
-                                        />
-                                    </Styles.Category>
-                                )
-                            })}
-                        </Styles.Categories>
+        <Styles.ContentWrapper>
+          <Styles.AddCategoryWrapper>
+            <Styles.AddCategory onClick={() => setEditorOpen(prev => !prev)}>
+              {isEditorOpen ? '-' : '+'}
+            </Styles.AddCategory>
+          </Styles.AddCategoryWrapper>
 
-                        {isEditorOpen && (
-                            <Editor
-                                inputState={categoryName}
-                                placeholder={'Название категории'}
-                                handleChange={handleChange}
-                                handleSave={handleSave}
-                                ref={reloadRef}
-                            />
-                        )}
-                    </Styles.ContentWrapper>
-                </Styles.Wrapper>
-            </Container>
-        </main>
-    );
+          <Styles.Categories>
+            {CATEGORIES.map(category => {
+              return (
+                <Styles.Category>
+                  <Styles.Title>
+                    {category.title}
+                  </Styles.Title>
+                  <SubCategories
+                    categoryId={category.id}
+                    subCategories={SubCategoriesData}
+                  />
+                </Styles.Category>
+              )
+            })}
+          </Styles.Categories>
+
+          {isEditorOpen && (
+            <Editor
+              inputState={categoryName}
+              placeholder={'Название категории'}
+              handleChange={handleChange}
+              handleSave={handleSave}
+              ref={reloadRef}
+            />
+          )}
+        </Styles.ContentWrapper>
+      </Container>
+    </Styles.Wrapper>
+  );
 };
