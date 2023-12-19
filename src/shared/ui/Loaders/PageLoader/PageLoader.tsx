69142fcf1f2e1fba2e50d569271d23119d48d36b
Amir Fayzullin <far20022002@mail.ru>
2023-12-19 23:58:44 +0500

    feat: changed icon and changed loader size

--- src/shared/ui/Loaders/PageLoader/PageLoader.tsx
+++ src/shared/ui/Loaders/PageLoader/PageLoader.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Spin} from "antd";
+import {LoadingOutlined} from "@ant-design/icons";
 
 import * as Styles from './PageLoader.styles';
 
 export const PageLoader = () => {
   return (
     <Styles.Wrapper>
-      <Spin size="large"/>
+      <Spin indicator={
+        <LoadingOutlined
+          style={{ fontSize: 120 }}
+          spin
+        />}
+      />
     </Styles.Wrapper>
   );
 };
