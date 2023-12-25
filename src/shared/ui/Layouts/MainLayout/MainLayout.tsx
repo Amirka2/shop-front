502693fd21dd74db51cff0b3c7c4061a262370fa
Amir Fayzullin <far20022002@mail.ru>
2023-12-25 15:35:49 +0500

    feat: added scroll on mainLayout mount

--- src/shared/ui/Layouts/MainLayout/MainLayout.tsx
+++ src/shared/ui/Layouts/MainLayout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from 'react';
+import React, {PropsWithChildren, useEffect} from 'react';
 
 import { TopBar } from "@/widgets";
 import { Footer } from "@/shared/components";
@@ -6,6 +6,10 @@ import { Footer } from "@/shared/components";
 import * as Styles from './MainLayout.styles';
 
 export const MainLayout = ({children, ...restProps}: PropsWithChildren) => {
+  useEffect(() => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
+
   return (
     <Styles.MainWrapper {...restProps} >
       <TopBar />
