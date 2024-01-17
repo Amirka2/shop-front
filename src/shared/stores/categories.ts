088d94455b8c012d1c4df4fabfb10e9e8586ae3d
Amir Fayzullin <far20022002@mail.ru>
2024-01-18 00:12:36 +0500

    feat: added AdminCategory field for creating category, added setter and getter for isLoading field

--- src/shared/stores/categories.ts
+++ src/shared/stores/categories.ts
@@ -1,13 +1,21 @@
 import { makeAutoObservable } from "mobx";
 import { ICategory } from "@/entities";
+import {categories} from "@/app/shop/mock";
 
 export class CategoriesStore {
     public categories: ICategory[] = [];
     public isLoading = false;
     public isInitialized = false;
 
+    private adminCategory: ICategory;
+
     constructor() {
         makeAutoObservable(this);
+        this.adminCategory = {
+            id: 0,
+            name: '',
+            photo: ''
+        }
     }
 
     set(categories: ICategory[]) {
@@ -16,5 +24,33 @@ export class CategoriesStore {
 
     get() {
         return this.categories;
+    }
+
+    public getIsLoading = () => {
+        return this.isLoading;
+    }
+
+    public setLoading = (value: boolean) => {
+        this.isLoading = value;
+    }
+
+    public setCategoryPhoto = (photoName: string) => {
+        this.adminCategory.photo = photoName;
+    }
+
+    public setCategoryName = (name: string) => {
+        this.adminCategory.name = name;
+    }
+
+    public getAdminCategory = () => {
+        return this.adminCategory;
+    }
+
+    public clearAdminCategory = () => {
+        this.adminCategory = {
+            id: 0,
+            name: '',
+            photo: ''
+        }
     }
 }
