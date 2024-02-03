d1756d9ec81bea19587827686995d2b17b260381
Amir Fayzullin <far20022002@mail.ru>
2024-02-03 16:06:25 +0500

    fix: fixed attr name

--- src/shared/ui/Icons/Minus/Minus.tsx
+++ src/shared/ui/Icons/Minus/Minus.tsx
@@ -7,7 +7,7 @@ export const Minus = ({
   return (
     <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...restProps}>
       <g>
-        <path id="Path 2" d="M20 12H4" stroke={color} stroke-linecap="round"/>
+        <path id="Path 2" d="M20 12H4" stroke={color} strokeLinecap="round"/>
       </g>
     </svg>
   );
