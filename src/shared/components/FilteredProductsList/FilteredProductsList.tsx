c557c08ada1373b9fe25343fbd4fba5996439440
Amir Fayzullin <far20022002@mail.ru>
2024-02-11 02:36:02 +0500

    feat: added spinner in search modal

--- src/shared/components/FilteredProductsList/FilteredProductsList.tsx
+++ src/shared/components/FilteredProductsList/FilteredProductsList.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect, useRef, useState} from "react";
+import {Spin} from "antd";
+import {LoadingOutlined} from "@ant-design/icons";
 
 import {getProducts} from "@/pages/Products/api";
 import {useStores} from "@/shared/hooks";
-import {ItemsGrid} from "@/shared/components";
+import {ItemsGrid, PagePlaceHolder} from "@/shared/components";
 import {Button} from "@/shared/ui";
 import {ProductCard} from "@/widgets";
 import {IProduct} from "@/entities";
@@ -10,68 +12,85 @@ import {IProduct} from "@/entities";
 import * as Styles from "./FilteredProductsList.styles";
 
 interface SearchProps {
-    searchInput: string;
-    setSearchInput: React.Dispatch<string>;
+  searchInput: string;
+  setSearchInput: React.Dispatch<string>;
 }
 
-export const FilteredProductsList = ({ searchInput, setSearchInput }: SearchProps) => {
-    const { searchStore } = useStores();
-    const { products } = searchStore;
-    const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([]);
-    const inputRef = useRef<HTMLInputElement>(null);
+export const FilteredProductsList = ({searchInput, setSearchInput}: SearchProps) => {
+  const {searchStore} = useStores();
+  const {products} = searchStore;
+  const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-    useEffect(() => {
-        if (inputRef.current) {
-            inputRef.current.focus();
-        }
-    }, [searchInput]);
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [searchInput]);
 
-    useEffect(() => {
-        const response = getProducts();
-        response.then(result => {
-            searchStore.set(result);
-            setFilteredProducts(result);
-        })
-    }, [])
+  useEffect(() => {
+    setLoading(true);
 
-    useEffect(() => {
-        setFilteredProducts(
-          products.filter((item) =>
-                item.name.toLowerCase().includes(searchInput.toLowerCase())
-            )
-        );
-    }, [searchInput, products]);
+    const response = getProducts();
+    response.then(result => {
+      searchStore.set(result);
+      setFilteredProducts(result);
+      setLoading(false);
+    })
+  }, [])
 
-    return (
-        <Styles.SearchWrapper>
-            <Styles.ModalInput
-                ref={inputRef}
-                placeholder={'Поиск'}
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)}
-                autoFocus
-            />
-            {filteredProducts.length > 0 ? (
-              <Styles.ProductsContainer>
-                  <ItemsGrid style={{
-                      padding: '0 20px',
-                      justifyContent: 'space-evenly'
-                  }}>
-                      {filteredProducts?.map((product) => (
-                        <ProductCard key={product.id} {...product} />
-                      ))}
-                  </ItemsGrid>
-              </Styles.ProductsContainer>
-            ) : (
-              <Styles.LabelContainer>
-                  <p>
-                      По данному запросу ничего не найдено
-                  </p>
-                  <Button size={"S"} onClick={() => setSearchInput('')}>
-                      Очистить запрос
-                  </Button>
-              </Styles.LabelContainer>
-            )}
-        </Styles.SearchWrapper>
+  useEffect(() => {
+    setFilteredProducts(
+      products.filter((item) =>
+        item.name.toLowerCase().includes(searchInput.toLowerCase())
+      )
     );
+  }, [searchInput, products]);
+
+  if (isLoading) {
+    return (
+      <PagePlaceHolder>
+        <Spin indicator={
+          <LoadingOutlined
+            style={{fontSize: 120}}
+            spin
+          />}
+        />
+      </PagePlaceHolder>
+    )
+  }
+
+  return (
+    <Styles.SearchWrapper>
+      <Styles.ModalInput
+        ref={inputRef}
+        placeholder={'Поиск'}
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.target.value)}
+        autoFocus
+      />
+      {filteredProducts.length > 0 ? (
+        <Styles.ProductsContainer>
+          <ItemsGrid style={{
+            padding: '0 20px',
+            justifyContent: 'space-evenly'
+          }}>
+            {filteredProducts?.map((product) => (
+              <ProductCard key={product.id} {...product} />
+            ))}
+          </ItemsGrid>
+        </Styles.ProductsContainer>
+      ) : (
+        <Styles.LabelContainer>
+          <p>
+            По данному запросу ничего не найдено
+          </p>
+          <Button size={"S"} onClick={() => setSearchInput('')}>
+            Очистить запрос
+          </Button>
+        </Styles.LabelContainer>
+      )}
+    </Styles.SearchWrapper>
+  );
 };
